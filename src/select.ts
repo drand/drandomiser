aa2b5bcc5f9cd37573d1d95af20d44b2c33b3b45
Patrick McClurg <patrick.mcclurg@protocol.ai>
2023-01-21 11:17:19 +0100

    support for drawing multiple values from the csv file

--- src/select.ts
+++ src/select.ts
@@ -1,3 +1,4 @@
+import {sha256} from "@noble/hashes/sha256"
 import {fetchBeacon, HttpCachingChain, HttpChainClient} from "drand-client"
 
 export type SelectionOptions = {
@@ -7,18 +8,46 @@ export type SelectionOptions = {
 }
 
 export async function select(options: SelectionOptions): Promise<string[]> {
+    // if we're picking equal or more values than exist, return them all
+    if (options.count >= options.values.length) {
+        return options.values
+    }
+
+    // let's get the chosen random number from drand
     const chain = new HttpCachingChain("https://api.drand.sh")
     const client = new HttpChainClient(chain)
     const beacon = await fetchBeacon(client, options.round)
 
-    const randomBytes = Buffer.from(beacon.randomness, "hex")
-    const someBigNumber = bufferToBigInt(randomBytes)
+    // We sort the values lexographically to ensure repeatability
+    // now we're going to hash the randomness for each draw we want to do and turn it into an index.
+    // We then draw the value for that index from our `remainingValues` array and repeat the process,
+    // remove it from that array, and repeat the process until we have no draws left to do
+    let remainingValues = options.values.slice().sort()
+    let remainingDraws = options.count
+    let currentRandomness: Uint8Array = Buffer.from(beacon.randomness, "hex")
+    let chosenValues: string[] = []
 
-    const selectedIndex = someBigNumber % BigInt(options.values.length)
+    while (remainingDraws > 0) {
+        currentRandomness = sha256.create().update(currentRandomness).digest()
+        const chosenIndex = indexFromRandomness(currentRandomness, remainingDraws)
 
-    const sortedValues = options.values.slice().sort()
-    return [sortedValues[Number(selectedIndex)]]
+        chosenValues = [...chosenValues, remainingValues[chosenIndex]]
+        remainingValues = [
+            ...remainingValues.slice(0, chosenIndex),
+            ...remainingValues.slice(chosenIndex + 1, remainingValues.length)
+        ]
+        remainingDraws--
+    }
+
+    return chosenValues
 }
+
+function indexFromRandomness(randomBytes: Uint8Array, totalEntryCount: number): number {
+    // should probably add a check that the entryCount is small enough to avoid modulo bias here
+    const someBigNumber = bufferToBigInt(randomBytes)
+    return Number(someBigNumber % BigInt(totalEntryCount))
+}
+
 
 function bufferToBigInt(buffer: Uint8Array): bigint {
     let output = BigInt(0)
